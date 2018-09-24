2fdac2bd6af172fcfd161cc424e267942a0ff731
hgreen-akl <h.green@auckland.ac.nz>
2018-09-24 14:43:21 +1200

    tweaks of checking script

--- R Scripts/Individual CSV checks.R
+++ R Scripts/Individual CSV checks.R
@@ -1,9 +1,27 @@
-# Method to import the csv 
-
 library(tidyverse)
 
-file_to_check <- file.choose()
+## Loading of the file containg the list of existing files. 
+filename_Database <- "G:\\Team Drives\\Research Team\\Projects\\2018 Driving Sim Reproducibility\\Participant Information.xlsx" 
+list_of_DAT_files <- read_xlsx(filename_Database, sheet = "Scenario Coding") %>% 
+  mutate(pattern_name = paste0(Participant,"_", Run_Number))
+
+list_of_DAT_files %>% head()
+
+## function to ask which number row to check
+
+number <- readline("What is the value of x?") %>% as.numeric()
+x <- list_of_DAT_files$pattern_name[readline("What is the value of x?") %>% as.numeric()]
+
+## if a checked file exists it will give an output to say so or else it will load a file to check
+
+file_to_check <- if(list_of_DAT_files$Scenario[number] == "Familiarisation"){ print("CSV file is a Familiarisation") 
+  } else {
+    list.files(path = "G:/Team Drives/Research Team/Projects/2018 Driving Sim Reproducibility/3_Raw Data/",
+                pattern = paste0("?",x,"_trimmed"), recursive = TRUE, full.names = TRUE)}
+
 csv_to_check <- read_csv(file_to_check, col_names = TRUE) %>% data.frame() %>% as_tibble()
+
+csv_to_check <- csv_to_check %>% mutate(session = rep(list_of_DAT_files$Session[number],times = nrow(csv_to_check)) ,scenario = rep(list_of_DAT_files$Scenario[number],times = nrow(csv_to_check)))
 
 to_plot <- csv_to_check %>% gather(Longitudinal_Veloc:Road_Curve, key = "Variable", value = "value")
 
@@ -56,4 +74,8 @@ check_data <- function() {
   plot_by_time
 }
 
-check_data()
+check_data <- csv_to_check
+
+save_location <- str_replace(file_to_check, "trimmed", "checked")
+
+write.csv(checked_data)
